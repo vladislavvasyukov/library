e27f640b868029bfd4e64d602ca2cfeae6a7fbf2
Vladislav <vladislav.vasyukof@gmail.com>
2018-05-26 17:20:23 +0300

    add button showListBooks

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -16,4 +17,22 @@ MainWindow::~MainWindow()
 void MainWindow::on_loginButton_clicked()
 {
     ui->label->setText("You are successfully login!!!");
+}
+
+void MainWindow::on_showListBooks_clicked()
+{
+    QMessageBox msgBox;
+    msgBox.setText("Question");
+    msgBox.setInformativeText("Are you really want come to world of books?");
+    msgBox.setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
+    msgBox.setIcon(QMessageBox::Information);
+    msgBox.setDefaultButton(QMessageBox::Ok);
+
+    int res = msgBox.exec();
+    if (res == QMessageBox::Ok){
+        QMessageBox::about(this, "OK", "You are reader");
+    } else {
+        QMessageBox::about(this, "BYE", "Goodbye, stupid");
+        QApplication::quit();
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -18,6 +18,8 @@ public:
 private slots:
     void on_loginButton_clicked();
 
+    void on_showListBooks_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
