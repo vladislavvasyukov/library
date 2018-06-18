9220d002b9116166be0715ca8809275fe525814b
Vladislav <vladislav.vasyukof@gmail.com>
2018-06-18 23:42:30 +0300

    add connect to database (fail)

--- addbook.cpp
+++ addbook.cpp
@@ -1,6 +1,7 @@
 #include "addbook.h"
 #include "ui_addbook.h"
 #include <QMessageBox>
+#include <QSqlDatabase>
 
 AddBook::AddBook(QWidget *parent) :
     QDialog(parent),
@@ -21,8 +22,18 @@ void AddBook::on_add_book_in_library_clicked()
     msgBox.setInformativeText("Here you must make request to database");
     msgBox.setIcon(QMessageBox::Information);
 
-    int res = msgBox.exec();
-    if (res == QMessageBox::Ok){
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setHostName("localhost");
+    db.setDatabaseName("library");
+    db.setUserName("root");
+    db.setPassword("robertheinlein1993");
+    bool isOpen = db.open();
+    if (isOpen){
+        int res = msgBox.exec();
+        if (res == QMessageBox::Ok){
+            this->close();
+        }
+    } else {
         this->close();
     }
 }
