10a2bdaf63e952f9137d4fec5412219d5f5246a0
Vladislav <vladislav.vasyukof@gmail.com>
2018-06-19 23:13:43 +0300

    Добавил подключение к SQLite

--- addbook.cpp
+++ addbook.cpp
@@ -2,6 +2,8 @@
 #include "ui_addbook.h"
 #include <QMessageBox>
 #include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QDebug>
 
 AddBook::AddBook(QWidget *parent) :
     QDialog(parent),
@@ -17,23 +19,33 @@ AddBook::~AddBook()
 
 void AddBook::on_add_book_in_library_clicked()
 {
+    QDebug() << "Start working window";
     QMessageBox msgBox;
     msgBox.setText("Information");
     msgBox.setInformativeText("Here you must make request to database");
     msgBox.setIcon(QMessageBox::Information);
 
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setHostName("localhost");
-    db.setDatabaseName("library");
-    db.setUserName("root");
-    db.setPassword("robertheinlein1993");
+    db.setDatabaseName("C:\my_projects\library.db");
     bool isOpen = db.open();
+    QSqlQuery query;
+    query.exec("select id, lname from readers");
+    QString message = "";
+    while (query.next()){
+        message += query.value(0).toString();
+        message += query.value(1).toString();
+        ui->textEdit->insertPlainText(message+"\n");
+    }
+
+
     if (isOpen){
         int res = msgBox.exec();
         if (res == QMessageBox::Ok){
+            QDebug() << "End working window";
             this->close();
         }
     } else {
+        QDebug() << "End working window";
         this->close();
     }
 }
