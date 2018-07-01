4b2cb4af780640a3b3a61c02fae666e19c3bc4df
Vladislav <vladislav.vasyukof@gmail.com>
2018-07-01 09:11:03 +0300

    add output of table 'readers' for deleting reader

--- deletereader.cpp
+++ deletereader.cpp
@@ -1,11 +1,25 @@
 #include "deletereader.h"
 #include "ui_deletereader.h"
+#include <QSqlDatabase>
+#include <QSqlTableModel>
 
 DeleteReader::DeleteReader(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::DeleteReader)
 {
     ui->setupUi(this);
+
+    QStringList headers = {"Имя", "Фамилия", "Дата рождения", "Адрес", "Телефон"};
+    QSqlTableModel *model = new QSqlTableModel();
+    model->setTable("readers");
+
+    for (int i = 1; i < model->columnCount(); i++) {
+        model->setHeaderData(i, Qt::Horizontal, headers[i-1]);
+    }
+    model->select();
+
+    ui->tableView->setModel(model);
+    ui->tableView->setColumnHidden(0, true);
 }
 
 DeleteReader::~DeleteReader()
