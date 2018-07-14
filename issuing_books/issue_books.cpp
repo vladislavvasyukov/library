7e95e192405668d645c215881107be2005d5499a
Vladislav <vladislav.vasyukof@gmail.com>
2018-07-14 10:10:21 +0300

    add functional of issuing books

--- issuing_books/issue_books.cpp
+++ issuing_books/issue_books.cpp
@@ -1,11 +1,24 @@
 #include "issue_books.h"
 #include "ui_issuedbooks.h"
+#include <QSqlTableModel>
 
 IssuedBooks::IssuedBooks(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::IssuedBooks)
 {
     ui->setupUi(this);
+
+    QStringList headersBooks = {"Автор", "Название", "ISBN", "Дата поступления", "Жанр"};
+    QSqlTableModel *model = new QSqlTableModel();
+    model->setTable("books");
+
+    for (int i = 1; i < model->columnCount(); i++) {
+        model->setHeaderData(i, Qt::Horizontal, headersBooks[i-1]);
+    }
+    model->select();
+
+    ui->tableView->setModel(model);
+    ui->tableView->setColumnHidden(0, true);
 }
 
 IssuedBooks::~IssuedBooks()
